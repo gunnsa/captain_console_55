90c19e1941b27976441d42b63a0442431aa5c374
gunnsa <54947830+gunnsa@users.noreply.github.com>
2020-05-04 17:01:39 +0000

    postgreSQL start, not ready

--- captain_console55/settings.py
+++ captain_console55/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'captain_console55.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ijaznmhb',
+        'USER': 'ijaznmhb',
+        'PASSWORD': 'MgZ5KMadt0SCXcvwkXsUBqXTiqg5FaMk',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
