9c7f7e8fc1de13795a8c20a38be76f259c785c6a
gunnsa <54947830+gunnsa@users.noreply.github.com>
2020-05-04 18:30:32 +0000

    migration

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_lengt=255)
+    name = models.CharField(max_length=255)
     manufacturer = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255)
     long_description = models.CharField(max_length=999, blank=True)
