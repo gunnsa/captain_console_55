78be26e8b44e25a03e4f0d2322a4156872a24e6b
veronikasif <54938148+veronikasif@users.noreply.github.com>
2020-05-05 17:45:58 +0000

    trying to fix cart

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,7 @@ from product.models import Product
 
 
 class Cart(models.Model):
-    products = models.ManyToManyField(Product, null=True, blank=True)
+    products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
