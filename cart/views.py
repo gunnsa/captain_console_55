12c056f1f32325cdeb5f56bd64456cfb24d0e006
gunnsa <54947830+gunnsa@users.noreply.github.com>
2020-05-06 22:30:35 +0000

    ni

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from cart.models import Cart
-from product.models import Product
 
 
 def index(request):
     context = {'carts': Cart.objects.all().filter(user_id=request.user.id)}
     return render(request, 'cart/index.html', context)
+
+def remove_cart_item(request):
+    #get all the items in the cart of the current user
+    #remove the item from the users cart
+
 
