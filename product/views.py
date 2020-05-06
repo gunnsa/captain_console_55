c75ddde76496d8295922b400b42929e942b4c705
monikasol <54938564+monikasol@users.noreply.github.com>
2020-05-06 00:16:33 +0000

    dropdown

--- product/views.py
+++ product/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-
 from product.models import Product
 
 # Create your views here.
 def index(request):
-    context = {'products': Product.objects.all().order_by('name')}
+    context = {'products': Product.objects.all()}
     return render(request, 'product/index.html', context)
 
 #/products/id
@@ -12,3 +11,9 @@ def get_product_by_id(request, id):
     return render(request, 'product/product_details.html', {
         'product': get_object_or_404(Product, pk=id)
     })
+
+def sort_product_by_specific(request, manufacturer):
+    context = {'products': Product.objects.all()}
+    return render(request, 'product/index.html', context)
+
+
