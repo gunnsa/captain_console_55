d3f09120c2e00652afe9b42a485d0d8fb8d39d1d
gunnsa <54947830+gunnsa@users.noreply.github.com>
2020-05-07 17:40:43 +0000

    total shit

--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,19 @@ from cart.models import Cart
 
 
 def index(request):
-    context = {'carts': Cart.objects.all().filter(user_id=request.user.id)}
+    usercart = Cart.objects.all().filter(user_id=request.user.id)
+    sumtotal = 0
+    eachItem = []
+    for product in usercart:
+        print(product.quantity)
+        total = product.quantity * product.product.price
+        print('total: ', total)
+        eachItem.append(total)
+        sumtotal += product.quantity * product.product.price
+        print(sumtotal)
+        print('eachitem ', eachItem)
+        #sum += carts.quantity * carts.product.price
+        context = {'carts': Cart.objects.all().filter(user_id=request.user.id), 'sumtotal': sumtotal, 'eachitem': eachItem}
     return render(request, 'cart/index.html', context)
 
 @csrf_exempt
