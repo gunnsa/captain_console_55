11a605566254fac0c250f41d6a38ad6e2f7c6efe
monikasol <54938564+monikasol@users.noreply.github.com>
2020-05-08 17:24:06 +0000

    mm

--- order/models.py
+++ order/models.py
@@ -1,19 +1,11 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.auth.models import User
-
 
 from cart.models import Cart
 from product.models import Product
 
 
 # Create your models here.
-
-
-class Order(models.Model):
-    order = models.ManyToManyField(Cart, on_delete=models.CASCADE)
-    current_order = models.BooleanField()
-    order_date = models.DateTimeField()
 
 
 
@@ -26,18 +18,3 @@ class Delivery(models.Model):
     city = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=3)
     additional_info = models.CharField(max_length=999)
-
-
-class PickUp(models.Model):
-    pickup_instore = models.BooleanField()
-
-
-class Payment(models.Model):
-    pass
-
-
-class ProcessedOrder(models.Model):
-    delivery_id = models.OneToOneField(Delivery, blank=True,on_delete=models.CASCADE)
-    pickup_id = models.OneToOneField(PickUp, blank=True,on_delete=models.CASCADE)
-    payment_id = models.OneToOneField(Payment, on_delete=models.CASCADE)
-    orders = models.OneToOneField(Order, on_delete=models.CASCADE)
