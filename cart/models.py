6dc1b5432c540e69fc68d6fc90911c3073ee85b6
monikasol <54938564+monikasol@users.noreply.github.com>
2020-05-08 14:12:14 +0000

    kk

--- cart/models.py
+++ cart/models.py
@@ -8,3 +8,16 @@ class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
+
+class Delivery(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    email_address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=7)
+    home_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=3)
+    additional_info = models.CharField(max_length=999)
+    order_date = models.FloatField()
+    order_time = models.FloatField()
+
