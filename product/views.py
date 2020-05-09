4d9ede0d85566c29e47db3c41dc5dea14669b8aa
monikasol <54938564+monikasol@users.noreply.github.com>
2020-05-09 23:09:10 +0000

    vhkj

--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Max
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
@@ -8,45 +9,21 @@ from user.models import User
 
 # Create your views here.
 def index(request):
-    if 'brand_filter' in request.GET:
-        drop_filter = request.GET['brand_filter']
-        products = [{
-            'id': x.id,
-            'name': x.name,
-            'color': x.color,
-            'price': x.price,
-            'short_description': x.short_description,
-            'manufacturer': x.manufacturer,
-            'color': x.color,
-            'firstImage': x.productimage_set.first().image
-        } for x in Product.objects.filter(manufacturer__exact=drop_filter)]
+    if 'min_price' in request.GET:
+        products = JsonResponse_form(Product.objects.all().order_by('price'))
         return JsonResponse({'data': products})
 
-    elif 'price' in request.GET:
-        products = [{
-            'id': x.id,
-            'name': x.name,
-            'color': x.color,
-            'price': x.price,
-            'short_description': x.short_description,
-            'manufacturer': x.manufacturer,
-            'color': x.color,
-            'firstImage': x.productimage_set.first().image
-        } for x in Product.objects.all().order_by('price')]
+    elif 'max_price' in request.GET:
+        products = JsonResponse_form(Product.objects.all().order_by('-price'))
+        return JsonResponse({'data': products})
+
+    elif 'name' in request.GET:
+        products = JsonResponse_form(Product.objects.all().order_by('name'))
         return JsonResponse({'data': products})
 
     elif 'search_filter' in request.GET:
         search_filter = request.GET['search_filter']
-        products = [{
-            'id': x.id,
-            'name': x.name,
-            'color': x.color,
-            'price': x.price,
-            'short_description': x.short_description,
-            'manufacturer': x.manufacturer,
-            'color': x.color,
-            'firstImage': x.productimage_set.first().image
-        } for x in Product.objects.filter(name__icontains=search_filter)]
+        products = JsonResponse_form(Product.objects.filter(name__icontains=search_filter))
         return JsonResponse({'data': products})
 
     products = Product.objects.all().order_by('name')
@@ -88,4 +65,34 @@ def add_to_cart(request, productid, quantity):
     return render(request, 'product/index.html', context)
 
 
+@csrf_exempt
+def sort_by_brand(request, manufacturer):
+    if 'min_price' in request.GET:
+        tilraun = JsonResponse_form(Product.objects.all().filter(manufacturer__exact=manufacturer).order_by('price'))
+        return JsonResponse({'data': tilraun})
 
+    elif 'max_price' in request.GET:
+        tilraun = JsonResponse_form(Product.objects.all().filter(manufacturer__exact=manufacturer).order_by('-price'))
+        return JsonResponse({'data': tilraun})
+
+    elif 'name' in request.GET:
+        tilraun = JsonResponse_form(Product.objects.all().filter(manufacturer__exact=manufacturer).order_by('name'))
+        return JsonResponse({'data': tilraun})
+
+    products = Product.objects.all().filter(manufacturer__exact=manufacturer)
+    context = {'products': products}
+    return render(request, 'product/index.html', context)
+
+
+def JsonResponse_form(request):
+    products = [{
+        'id': x.id,
+        'name': x.name,
+        'color': x.color,
+        'price': x.price,
+        'short_description': x.short_description,
+        'manufacturer': x.manufacturer,
+        'color': x.color,
+        'firstImage': x.productimage_set.first().image
+    } for x in request]
+    return products
