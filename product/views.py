f4284db3a864cb937696155fc987bfbc941fd654
gunnsa <54947830+gunnsa@users.noreply.github.com>
2020-05-09 15:44:51 +0000

    Update views.py

--- product/views.py
+++ product/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from cart.models import Cart, CartTest
 from product.models import Product
+from user.models import User
 
 # Create your views here.
 def index(request):
@@ -90,12 +91,24 @@ def add_to_cart_geyma(request, productid, quantity):
 def add_to_cart(request, productid, quantity):
     if request.method == 'POST':
         current_user = request.user.id
-        if CartTest.objects.filter(user_id__exact=current_user, product_id=productid):
-            existing_cart = CartTest.objects.get(product_id=productid, user_id=current_user)
-            print(existing_cart.quantity)
-            existing_cart.quantity = existing_cart.quantity + 1
-            print(existing_cart.quantity)
-            existing_cart.save()
+        userproduct = Product.objects.filter(pk=productid)
+        if CartTest.objects.filter(user_id=current_user):
+            print('fundið')
+            current_cart= CartTest.objects.get(user_id=current_user)
+            product_to_add = Product()
+            current_cart.product.add(userproduct)
         else:
-            CartTest.objects.create(user_id=current_user, product_id=productid, quantity=quantity)
+            userinstance = User.objects.get(pk=current_user)
+            instance = CartTest.objects.create(user=userinstance, quantity=quantity)
+            instance.product.set(userproduct)
+    context = {'products': Product.objects.all().order_by('name')}
+    return render(request, 'product/index.html', context)
 
+
+
+Yuji's answer is correct. You can not add an object to a M2M field until it has been saved. I wanted to mention a shorter way to create instances though.
+
+user = User.objects.get(username=uname)
+flst = FollowerList(follower=user) #use key word args to assign fields
+flst.save()
+flst.followed.add(user)
