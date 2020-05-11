7fa49b75393c4e9a2a4111afec17b599ac85c964
gunnsa <54947830+gunnsa@users.noreply.github.com>
2020-05-11 17:43:43 +0000

    upd

--- static/js/update_cart_quantity.js
+++ static/js/update_cart_quantity.js
@@ -1,8 +1,47 @@
-//fá þegar quantity er hækkað, matcha við cart Id og skrifa aftur niður í db nýtt quantity, verðið þarf
-//líka að uppfærast
+$(document).ready(function () {
+    $('.decrease_cart').on('click', function(e){
+      e.preventDefault();
+      var parent_value = this.parentNode.parentNode
+      child_value = parent_value.childNodes
+
+      var value = parseInt(child_value[3].value, 10);
+      value = isNaN(value) ? 0 : value;
+
+      if (value == 1) {
+          value = 1
+      }
+      else {
+          value--;
+      }
+      child_value[3].value = value;
+      child_value[3].innerHTML = value; //þannig við sjáum breytingu í HTML til að tengja við cart
+    })
+})
 
 $(document).ready(function () {
-    $('#decrease.decrease_value').on('click', function (e) {
+    $('.increase_cart').on('click', function(e){
+      e.preventDefault();
+      var parent_value = this.parentNode.parentNode
+      child_value = parent_value.childNodes
+
+      var value = parseInt(child_value[3].value, 10);
+      console.log(value)
+
+      value = isNaN(value) ? 0 : value;
+      value++;
+
+      child_value[3].value = value;
+      child_value[3].innerHTML = value; //þannig við sjáum breytingu í HTML til að tengja við cart
+    })
+})
+
+
+
+
+
+
+$(document).ready(function () {
+    $('.decrease_cart').on('click', function (e) {
         e.preventDefault();
         var cartid = $(this).attr('data-id');
         console.log('cartId', cartid)
@@ -23,8 +62,14 @@ $(document).ready(function () {
 
             }
         });
+
+        if (window.location.href == 'http://127.0.0.1:8000/cart/'){
+        }
     })
 });
+
+//fá þegar quantity er hækkað, matcha við cart Id og skrifa aftur niður í db nýtt quantity, verðið þarf
+//líka að uppfærast
 
 
 
