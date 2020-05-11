4294ed392c21e373cb689d0be9a7e0f61c70b300
elisaeik <54938207+elisaeik@users.noreply.github.com>
2020-05-11 15:30:26 +0000

    update

--- static/js/quantity_of_product.js
+++ static/js/quantity_of_product.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
     $('.decrease_value').on('click', function(e){
       e.preventDefault();
+      var parent_value = this.parentNode.parentNode
+      child_value = parent_value.childNodes
 
-      var DEC_cartId_this = $(this).attr('data-id');
-      console.log('DEC_cartId_this: ', DEC_cartId_this);
-
-      var DEC_cartid_qty = document.getElementById('quantity').getAttribute('data-id');
-      console.log('DEC_cartid_qty: ', DEC_cartid_qty);
-      var value = parseInt(document.getElementById('quantity').value, 10);
-
+      var value = parseInt(child_value[3].value, 10);
+      console.log(value)
       value = isNaN(value) ? 0 : value;
 
       if (value == 1) {
@@ -17,26 +14,25 @@ $(document).ready(function () {
       else {
           value--;
       }
-      document.getElementById('quantity').value = value;
-      document.getElementById('quantity').innerHTML = value; //þannig við sjáum breytingu í HTML til að tengja við cart
+      child_value[3].value = value;
+      child_value[3].innerHTML = value; //þannig við sjáum breytingu í HTML til að tengja við cart
     })
 })
 
 $(document).ready(function () {
     $('.increase_value').on('click', function(e){
       e.preventDefault();
+      var parent_value = this.parentNode.parentNode
+      child_value = parent_value.childNodes
 
-      var cartId_this = $(this).attr('data-id');
-      console.log('cartId: ', cartId_this);
+      var value = parseInt(child_value[3].value, 10);
+      console.log(value)
 
-      var cartid_qty = document.getElementById('quantity').getAttribute('data-id');
-      console.log('cartid_id: ', cartid_qty);
-
-      var value = parseInt(document.getElementById('quantity').value,10);
       value = isNaN(value) ? 0 : value;
       value++;
-      document.getElementById('quantity').value = value;
-      document.getElementById('quantity').innerHTML = value; //þannig við sjáum breytingu í HTML til að tengja við cart
+
+      child_value[3].value = value;
+      child_value[3].innerHTML = value; //þannig við sjáum breytingu í HTML til að tengja við cart
     })
 })
 
