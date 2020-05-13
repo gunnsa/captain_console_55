6a04ac077d9265e59681380080f5335e72caf9c1
veronikasif <54938148+veronikasif@users.noreply.github.com>
2020-05-13 13:27:19 +0000

    Update urls.py

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # http://localhost:8000/wishlist
-    path('', views.index, name="wishlist-index"),
-    path('<int:cartid>/remove_cart_item', views.remove_cart_item),
-    path('<int:cartid>/update_cart/<int:quantity>', views.update_cart)
+    #path('', views.index, name="wishlist-index"),
+    #path('<int:cartid>/remove_cart_item', views.remove_cart_item),
+    #path('<int:cartid>/update_cart/<int:quantity>', views.update_cart)
 ]
